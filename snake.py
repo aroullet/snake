1d71dd65922747d8f4a246f3cefc355e55c0d1cf
Antonin Roullet <antonin.roullet@protonmail.com>
2022-04-24 11:01:08 +0200

    Initial commit, first draft of Snake class complete.

---
+++ snake.py
@@ -0,0 +1,77 @@
+from enum import Enum, auto
+from point import Point
+
+
+class Directions(Enum):
+    UP = auto()
+    DOWN = auto()
+    LEFT = auto()
+    RIGHT = auto()
+
+
+class Snake:
+    speed = 10
+
+    def __init__(self, initial_pos: list[Point]):
+        self.position = initial_pos
+        self._length = len(initial_pos)
+        self._direction = Directions.RIGHT
+
+    def __str__(self):
+        return f'{self.position}'
+
+    def update_position(self):
+        """
+        Iterates over position array from tail to head, setting position of block i to that of block i+1
+        """
+        for i in range(self._length-1):
+            self.position[i] = self.position[i+1]
+        self._update_head()
+
+    def _update_head(self):
+        head = self.position[-1]
+        new_x, new_y = head.x, head.y
+
+        if self._direction == Directions.UP:
+            new_y += 1
+        elif self._direction == Directions.DOWN:
+            new_y -= 1
+
+        elif self._direction == Directions.RIGHT:
+            new_x += 1
+
+        elif self._direction == Directions.LEFT:
+            new_x -= 1
+
+        self.position[-1] = Point(new_x, new_y)
+
+    def grow(self):
+        self._length += 1
+
+    def move_up(self):
+        if self._direction != Directions.DOWN:
+            self._direction = Directions.UP
+
+    def move_down(self):
+        if self._direction != Directions.UP:
+            self._direction = Directions.DOWN
+
+    def move_left(self):
+        if self._direction != Directions.RIGHT:
+            self._direction = Directions.LEFT
+
+    def move_right(self):
+        if self._direction != Directions.LEFT:
+            self._direction = Directions.RIGHT
+
+
+if __name__ == '__main__':
+
+    pos = [Point(0, -1), Point(0, 0), Point(0, 1)]
+    snake = Snake(pos)
+    print(snake)
+    snake.update_position()
+    print(snake)
+    snake.move_up()
+    snake.update_position()
+    print(snake)
